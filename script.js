45e761caeb165585bc8add7bb4704473fd9590c3
jlopez1011 <61877541+jlopez1011@users.noreply.github.com>
2020-03-06 09:42:12 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("button").click(function(){
+    let present=$(".class1").val();
+    let reason=$(".class2").val();
+    let age=$(".class3").val();
+    let name=$(".class4").val();
+    
+    
+    $("p").text('In the year 2050,${name}will ${nightmare} ${Number(number)*2} times.');
+});
